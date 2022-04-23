2aebf7c53a63e1d791ba85ebef72e3d7e8b4580a
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-24 00:55:52 +0200

    die Lösung habe ich auskommentiert, weil es eine bessere gibt. Mithilfe von MongoRepository

--- src/main/java/de/mongodb/springmongodbtutorial/SpringMongoDbTutorialApplication.java
+++ src/main/java/de/mongodb/springmongodbtutorial/SpringMongoDbTutorialApplication.java
@@ -10,6 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 @SpringBootApplication
 public class SpringMongoDbTutorialApplication {
@@ -19,20 +22,44 @@ public class SpringMongoDbTutorialApplication {
   }
 
   @Bean
-  CommandLineRunner commandLineRunner (StudentRepository repository){
+  CommandLineRunner commandLineRunner (StudentRepository repository, MongoTemplate mongoTemplate){
     return args -> {
       Address address = new Address("Germany", "GD12", "Dusseldorf");
+      String email = "joe@web.de";
       Student student = new Student(
           "Joseph",
           "Alzieb",
-          "joe@web.de",
+          email,
           Gender.MALE,
           address,
           List.of("Math", "Bio"),
           LocalDateTime.now()
       );
 
-      repository.insert(student);
+//      usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
+
+
     };
+  }
+
+
+  private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email,
+      Student student) {
+    Query query = new Query();
+    query.addCriteria(Criteria.where("email").is(email));
+
+    List<Student> students = mongoTemplate.find(query, Student.class);
+
+    if(students.size() >= 1){
+      throw new IllegalStateException("found many students with email " + email);
+    }
+
+    if(students.isEmpty()){
+      repository.insert(student);
+      System.out.println("Inserting student " + student);
+    }
+    else {
+      System.out.println(student + " already exists");
+    }
   }
 }
