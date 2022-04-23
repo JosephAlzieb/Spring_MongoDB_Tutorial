648850804025c5137fc934e5e63a09954158792f
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-23 20:04:12 +0200

    initial project

---
+++ src/main/java/de/mongodb/springmongodbtutorial/SpringMongoDbTutorialApplication.java
@@ -0,0 +1,13 @@
+package de.mongodb.springmongodbtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDbTutorialApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMongoDbTutorialApplication.class, args);
+  }
+
+}
