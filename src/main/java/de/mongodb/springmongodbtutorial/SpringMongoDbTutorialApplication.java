1c2315e75da59f8785a3befcd9cb2512fbc94d92
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-23 20:54:12 +0200

    CommandLineRunner um das Repos zu testen. nun kann man Dokumente (student) in der Collection (database) speichern

--- src/main/java/de/mongodb/springmongodbtutorial/SpringMongoDbTutorialApplication.java
+++ src/main/java/de/mongodb/springmongodbtutorial/SpringMongoDbTutorialApplication.java
@@ -1,7 +1,15 @@
 package de.mongodb.springmongodbtutorial;
 
+import de.mongodb.springmongodbtutorial.models.Address;
+import de.mongodb.springmongodbtutorial.models.Gender;
+import de.mongodb.springmongodbtutorial.models.Student;
+import de.mongodb.springmongodbtutorial.repositories.StudentRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMongoDbTutorialApplication {
@@ -10,4 +18,21 @@ public class SpringMongoDbTutorialApplication {
     SpringApplication.run(SpringMongoDbTutorialApplication.class, args);
   }
 
+  @Bean
+  CommandLineRunner commandLineRunner (StudentRepository repository){
+    return args -> {
+      Address address = new Address("Germany", "GD12", "Dusseldorf");
+      Student student = new Student(
+          "Joseph",
+          "Alzieb",
+          "joe@web.de",
+          Gender.MALE,
+          address,
+          List.of("Math", "Bio"),
+          LocalDateTime.now()
+      );
+
+      repository.insert(student);
+    };
+  }
 }
